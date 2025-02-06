02be1190cf20ea2378f6846146feafe89b6337a5
triciahutton <triciahuttonn@gmail.com>
2025-02-05 15:39:10 -0900

    Add files via upload

---
+++ update_and_run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+LOG_FILE="/glade/derecho/scratch/phutton/CMIP6/miroc6/ssp370/automation/log_update_and_run.log"
+echo "sarting script" >> "$LOG_FILE"
+
+# Read the START_DATE from the log file
+START_DATE=$(grep -oP 'Run Start Date: \K\d{8}' /glade/derecho/scratch/phutton/CMIP6/miroc6/ssp370/automation/log_date_file.log)
+
+if [ -z "$START_DATE" ]; then 
+	echo "ERROR: START_DATE not found in log_date_file.log"  >> "$LOG_FILE" 
+	exit 1
+fi
+
+NEW_START_DATE=$(date -d "$START_DATE + 151 days" +%Y%m%d)
+
+echo "Updated START_DATE: $NEW_START_DATE"  >> "$LOG_FILE"
+
+sed -i "s/^START_DATE='.*'/START_DATE='$NEW_START_DATE'/" run_extvar.sh
+echo "Changed start date in run_extvar.sh" >> "$LOG_FILE"
+
+echo "Now run run_extvar.sh"  >> "$LOG_FILE"
+
+
+qsub -V run_extvar.sh #need to add the log file from before test_WRF_extractvar.
