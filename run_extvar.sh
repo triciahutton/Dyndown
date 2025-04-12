fbdf5a7c75b297e79658216335bdc900cc992ac2
triciahutton <triciahuttonn@gmail.com>
2025-04-11 23:27:33 -0400

    Update run_extvar.sh

--- run_extvar.sh
+++ run_extvar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #PBS -A WYOM0200
-#PBS -N test_WRF_extractvar
+#PBS -N year_WRF_extractvar
 #PBS -l walltime=10:00:00
 #PBS -q main
 #PBS -j oe
@@ -11,12 +11,17 @@ source ~/miniconda3/bin/activate
 conda activate dyndown
 
 WRFDIR="/glade/campaign/uwyo/wyom0200/alaska/miroc6/ssp370/out-s1978"
-OUTDIR="/glade/derecho/scratch/phutton/CMIP6/miroc6/ssp370/automation/testout_daily"
+OUTDIR="/glade/derecho/scratch/phutton/CMIP6/miroc6/ssp370/post_proc-tpl/post_proc_out"
 
-START_DATE='19790502'
-NDAYS=15
-NUM_RUNS=3
+START_DATE='19810701'
+NDAYS=7
+NUM_RUNS=1
 
+LOG_FILE="/glade/derecho/scratch/phutton/CMIP6/miroc6/ssp370/post_proc-tpl/log_date_file.log"
+exec >  /glade/derecho/scratch/phutton/CMIP6/miroc6/ssp370/post_proc-tpl/log_date_file.log 2>&1
+echo "Run Start Date: $START_DATE" >> "$LOG_FILE"
+
+exec > "test_WRF_extractvar.${PBS_JOBID}" 2>&1
 
 for i in $(seq 1 $NUM_RUNS); do
 	python extract_vars_pool_tricia.py -w $WRFDIR --yrmd $START_DATE -o $OUTDIR --ndays $NDAYS
